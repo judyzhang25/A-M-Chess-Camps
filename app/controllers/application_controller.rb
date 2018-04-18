1c89f6940890fa62c864184eb179beb74bada20d
Judy Zhang <judyz@andrew.cmu.edu>
2018-04-18 13:56:48 +0000

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,30 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  # just show a flash message instead of full CanCan exception
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "You are not authorized to take this action."
+    redirect_to home_path
+  end
+
+  # handle 404 errors with an exception as well
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    flash[:error] = "Page cannot be found."
+    redirect_to home_path
+  end
+  
+  private
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def logged_in?
+    current_user
+  end
+  helper_method :logged_in?
+
+  def check_login
+    redirect_to login_url, alert: "You need to log in to view this page." if current_user.nil?
+  end
 end
